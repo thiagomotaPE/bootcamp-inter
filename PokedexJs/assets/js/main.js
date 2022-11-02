d244d4bb0b658bca94fe31d72ecb303e0342bd7f
mota <thiagomotape@hotmail.com.br>
2022-11-02 18:21:31 -0300

    fiz a primeira requisição com a pokeAPI, listei os 20 primeiros pokemons

--- PokedexJs/assets/js/main.js
+++ PokedexJs/assets/js/main.js
@@ -1,1 +1,9 @@
-console.log("testeeeee");
+
+const offset = 0
+const limit = 0
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => console.log(jsonBody))
+    .catch((error) => console.log(error))
