fae6f5cb26e037356c03b10ca40e9546cdbc1e06
mota <thiagomotape@hotmail.com.br>
2022-11-03 18:10:07 -0300

    reparei o erro com o innerHtml, deixei o codigo mais limpo e mais pequeno, fiz a requisição da listas de caracterisiticas do pokemon e mostrei na tela, aprendi o .join

--- PokedexJs/assets/js/main.js
+++ PokedexJs/assets/js/main.js
@@ -1,33 +1,23 @@
 
-const offset = 0
-const limit = 0
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+function convertPokemonTypesToLi(pokemonTypes){
+    return pokemonTypes.map((typeSlot) => `<li class="type"> ${typeSlot.type.name}</li>`)
+}
 
 function convertPokemonToLi(pokemon){
     return `<li class="pokemon">
-                <span class="number">#001</span>
+                <span class="number">#${pokemon.order}</span>
                 <span class="name">${pokemon.name}</span>
                 <div class="detail">
                     <ol class="types">
-                        <li class="type">grass</li>
-                        <li class="type">poison</li>
+                        ${convertPokemonTypesToLi(pokemon.types).join(' ')}
                     </ol>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+                    <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
                 </div>
             </li>`
 }
 
-const pokemonList = console.log(document.getElementById('pokemonList'))
+const pokemonList = document.getElementById('pokemonList')
 
-
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            pokemonList. += convertPokemonToLi(pokemon) //PROBLEMA AQUI
-            
-        }
-    })
-    .catch((error) => console.log(error))
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+})
