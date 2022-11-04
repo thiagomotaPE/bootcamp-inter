fb548688806ef295964b01e76b910ba3064bdc19
mota <thiagomotape@hotmail.com.br>
2022-11-04 17:51:26 -0300

    comecei a fazer a tela de especificação do pokemon

--- PokedexJs/assets/js/main.js
+++ PokedexJs/assets/js/main.js
@@ -10,19 +10,22 @@ function loadPokemonItens(offset, limit){
     
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
         const newHtml = pokemons.map((pokemon) => 
-            `<li class="pokemon ${pokemon.type}">
+            `<a href="pokemon-detail.html" class="linkPokemonDetail">
+                <li class="pokemon ${pokemon.type}">
 
-                <span class="number">#${pokemon.number}</span>
-                <span class="name">${pokemon.name}</span>
+                    <span class="number">#${pokemon.number}</span>
+                    <span class="name">${pokemon.name}</span>
 
-                <div class="detail">
-                    <ol class="types">
-                            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                    </ol>
+                    <div class="detail">
+                        <ol class="types">
+                                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                        </ol>
 
-                     <img src="${pokemon.photo}" alt="${pokemon.name}">
-                </div>
-            </li>`).join('')
+                        <img src="${pokemon.photo}" alt="${pokemon.name}">
+                    </div>
+                </li>
+            </a>
+            `).join('')
 
             pokemonList.innerHTML += newHtml
     })
