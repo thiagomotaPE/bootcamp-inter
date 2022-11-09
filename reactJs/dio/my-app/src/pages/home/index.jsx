90255692098a76f05f5a69de66dd28426e68a33b
mota <thiagomotape@hotmail.com.br>
2022-11-09 19:34:56 -0300

    comecei a fazer a pagina da dio com react, criei o componente Button e estilizei-o com styled-components e importei o botão´para o arquivo Home

--- reactJs/dio/my-app/src/pages/home/index.jsx
+++ reactJs/dio/my-app/src/pages/home/index.jsx
@@ -1,8 +1,11 @@
 import {Link} from 'react-router-dom';
+import { Button } from '../../components/Button';
 
 const Home = () => {
     return (<>
         <h1>Home</h1>
+        <Button title="teste1"/>
+        <Button title="teste2" variant="secundary" />
         <Link to="/Login">Fazer Login</Link>
         </>
     )
