620cb8a8356d8c03ea4529f1d7d88ad8d4a39d25
mota <thiagomotape@hotmail.com.br>
2023-01-24 19:51:53 -0300

    Update Carro.java
    criei os getters and setters da classe carro

--- Back-End/EstudosPoo-DIO/src/Poo/Carro.java
+++ Back-End/EstudosPoo-DIO/src/Poo/Carro.java
@@ -1,7 +1,35 @@
 package Poo;
 
 public class Carro {
-    String cor;
-    String modelo;
-    int capacidadeTanque;
+
+    //atributos da classe
+    private String cor;
+    private String modelo;
+    private int capacidadeTanque;
+
+
+    //getters ad setters
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getCapacidadeTaque() {
+        return capacidadeTanque;
+    }
+
+    public void setCapacidadeTaque(int capacidadeTanque) {
+        this.capacidadeTanque = capacidadeTanque;
+    }
 }
