ba64cbf4902ef79089788a5990b139c85b56bdbf
mota <thiagomotape@hotmail.com.br>
2023-01-25 17:59:00 -0300

    update

--- Back-End/Empresa/src/edu/Main.java
+++ Back-End/Empresa/src/edu/Main.java
@@ -6,8 +6,12 @@ public class Main {
         Funcionario funcionario = new Funcionario();
 
         Funcionario gerente = new Gerente();
+        Funcionario vendedor = new Vendedor();
+        Funcionario faxineiro = new Faxineiro();
 
-
-
+        Vendedor vendedor2 = (Vendedor) new Funcionario(); //não funcionou
+        /*o downcast deve ser evitado porque como a atributos na classe filha que serão
+        * retiradas para que o objeto tenha só os atributos da classe mae o java não sabe
+        * muito bem oque fazer com esses dados que sobraram por isso da erro.*/
     }
 }
