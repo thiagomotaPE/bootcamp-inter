91f453a180dad273078e50e300f98b9fc3b8cc00
mota <thiagomotape@hotmail.com.br>
2023-02-04 20:15:26 -0300

    UPDATE
    conclusao do modulo de SET

--- Back-End/Collections/Set/src/Main.java
+++ Back-End/Collections/Set/src/Main.java
@@ -1,6 +1,4 @@
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,8 +13,65 @@ public class Main {
         na ordem de inserção e os elementos repetidos só
         entram na lista uma vez*/
 
+        /*a biblioteca SET não trabalha com o index dos itens
+        * do conjunto(lista).
+        * então não da pra fazer nenhuma ação que precise saber
+        * a posição do item*/
 
-        //Exibindo a posição de algum elemento especifico
-        System.out.println("posição da nota 5.0: ");
+
+        //conferindo se um valor especifico esta  no conjunto
+        System.out.println("o conjunto 'notas' possui alguma nota 5.0 ? " + notas.contains(5d));
+
+        //exibindo a menor nota
+        System.out.println("a menor nota é: " + Collections.min(notas));
+
+        //exibindo a maior nota
+        System.out.println("a maior nota é: " + Collections.max(notas));
+
+        //exibindo a soma de todos os valores
+        Iterator<Double> iterator = notas.iterator();
+        Double soma = 0d;
+        while(iterator.hasNext()) {
+            Double next = iterator.next();
+            soma += next;
+        }
+        System.out.println("soma dos valores: " + soma);
+
+        //exibindo a media das notas
+        System.out.println("media de todas as notas:" + (soma / notas.size()));
+
+        //removendo um valor do conjunto
+        System.out.println("remova a nota 0: ");
+        notas.remove(0d);
+        System.out.println(notas);
+
+
+        //removendo notas menores que 7
+        //System.out.println("remova nota menores que 7: ");
+        //Iterator<Double> iterator1 = notas.iterator();
+        //while (iterator1.hasNext()) {
+        //     Double next = iterator.next();
+        //     if (next < 7d) iterator1.remove();
+        //}
+        //System.out.println(notas);
+
+        //mostraNDO O CONJUNTO NA ORDEM DE INSERÇÃO
+
+
+        //exibindo na ordem crescente
+        Set<Double> notas3 = new TreeSet<>(notas);
+        System.out.println("ordem crescente: " + notas3);
+
+        // apague todo conjunto
+        notas.clear();
+        System.out.println(notas);
+
+        //confira se o conjunto apagado esta vazio
+        System.out.println("apagou? " + notas.isEmpty());
+
+
+        //ORDENANDO O CONJUNTO
+        
     }
+
 }
